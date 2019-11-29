0f6ed0f2d4a53381b11d8a6b5f06b2460a66ce59
xiaqiulei <xiaqiulei@126.com>
2019-11-29 15:22:38 +0800

    init

---
+++ src/test/java/cn/commons/easycaptcha/EasyCaptchaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.commons.easycaptcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCaptchaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
